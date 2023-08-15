16d2b1a796dc2e521870b34136d28b2b97e66ac6
Siriuszx <114768817+Siriuszx@users.noreply.github.com>
2023-08-15 18:41:28 +0200

    Add submit book function

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const bookContainer = document.querySelector('.book-container');
 
+const bookModal = document.querySelector('#book-dialog');
+const openBookModal = document.querySelector('#open-modal');
+
+const formBookTitle = document.querySelector('#book-title');
+const formBookAuthor = document.querySelector('#book-author');
+const formBookPageCount = document.querySelector('#book-page-count');
+const formBookStatus = document.querySelector('#book-status');
+const submitBtn = document.querySelector('.submit-new-book');
+
 let myLibrary = [];
 
 function Book(title, author, pageCount, status) {
@@ -9,18 +18,6 @@ function Book(title, author, pageCount, status) {
     this.status = status;
 }
 
-// create new book obj --> create new book el based on book obj -->
-// add new book to library arr and dom list
-
-/*
-Create a new book
-Add it to the array as new Book object
-Empty book container node
-Loop through library array
-    Create a new book DOM element
-    Append it to the container
-*/
-
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
@@ -29,12 +26,19 @@ function createNewBook(title, author, pageCount, status) {
     return new Book(title, author, pageCount, status);
 }
 
-function updateDOMBookList() {
+function updateBookList() {
     bookContainer.innerHTML = '';
 
     myLibrary.forEach(el => {
         bookContainer.appendChild(createNewBookEl(el.title, el.author, el.pageCount, el.status));
     });
+}
+
+function submitNewBookHandler(e) {
+    let bookStatus = formBookStatus.value === 'on' ? true : false;
+    let newBook = createNewBook(formBookTitle.value,formBookAuthor.value,formBookPageCount.value,bookStatus);
+    addBookToLibrary(newBook);
+    updateBookList();
 }
 
 function removeBook() {
@@ -43,10 +47,10 @@ function removeBook() {
     for (let i = 0; i < myLibrary.length; i++) {
         if (myLibrary[i].title === parentEl.getAttribute('data-title')) {
             myLibrary.splice(i, 1);
-            parentEl.remove();   
+            parentEl.remove();
         }
     }
-    updateDOMBookList();
+    updateBookList();
 }
 
 function createNewBookEl(title, author, pageCount, status) {
@@ -98,10 +102,15 @@ function createNewBookEl(title, author, pageCount, status) {
     return newBook;
 }
 
+openBookModal.addEventListener('click', () => {
+    bookModal.showModal();
+});
+
+submitBtn.addEventListener('click', submitNewBookHandler);
+
 addBookToLibrary(createNewBook('Atmic Habits', 'James Clear', 320, true));
-updateDOMBookList();
 addBookToLibrary(createNewBook('Amic Habits', 'James Clear', 320, true));
 addBookToLibrary(createNewBook('Atomi Habits', 'James Clear', 320, true));
 addBookToLibrary(createNewBook('Atomic Habis', 'James Clear', 320, true));
 addBookToLibrary(createNewBook('Atomic Habits', 'James Clear', 320, true));
-updateDOMBookList();
+updateBookList();
