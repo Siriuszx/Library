e0b5c38f3086718c8f55127845bbdf36d12f20e6
Siriuszx <114768817+Siriuszx@users.noreply.github.com>
2023-08-15 20:01:22 +0200

    Refactor add book logic

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const formBookStatus = document.querySelector('#book-status');
 const submitBtn = document.querySelector('.submit-new-book');
 
 let myLibrary = [];
+// Last deleted uid?
 
 function Book(title, author, pageCount, status) {
     this.title = title;
@@ -18,24 +19,14 @@ function Book(title, author, pageCount, status) {
     this.status = status;
 }
 
-Book.prototype.toggleStatus = function() {
+Book.prototype.toggleStatus = function () {
     this.status = !this.status;
 };
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-
-function createNewBook(title, author, pageCount, status) {
-    return new Book(title, author, pageCount, status);
-}
-
-function updateBookList() {
-    bookContainer.innerHTML = '';
-
-    myLibrary.forEach(el => {
-        bookContainer.appendChild(createNewBookEl(el.title, el.author, el.pageCount, el.status));
-    });
+function addBookToLibrary(title, author, pageCount, status) {
+    let newBook = new Book(title, author, pageCount, status);
+    myLibrary.push(newBook);
+    bookContainer.appendChild(createNewBookEl(newBook, myLibrary.length)); // UID TODO
 }
 
 function clearFrom() {
@@ -46,43 +37,31 @@ function clearFrom() {
 }
 
 function submitNewBookHandler(e) {
+    //TODO
     let bookStatus = formBookStatus.value === 'on' ? true : false;
-    let newBook = createNewBook(formBookTitle.value,formBookAuthor.value,formBookPageCount.value,bookStatus);
+    let newBook = createNewBook(formBookTitle.value, formBookAuthor.value, formBookPageCount.value, bookStatus, myLibrary.length);
     addBookToLibrary(newBook);
     clearFrom();
-    updateBookList();
 }
 
 function removeBook() {
-    let parentEl = this.parentNode.parentNode;
-
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].title === parentEl.getAttribute('data-title')) {
-            myLibrary.splice(i, 1);
-            parentEl.remove();
-        }
-    }
-    updateBookList();
+    let bookEl = this.parentNode.parentNode;
+    myLibrary.splice(Number(bookEl.getAttribute('data-uid')), 1);
+    bookEl.remove();
 }
 
 function toggleStatus() {
-    let parentEl = this.parentNode.parentNode;
-    
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].title === parentEl.getAttribute('data-title')) {
-            myLibrary[i].toggleStatus();
-        }
-    }
-    updateBookList();
+    //TODO
 }
 
-function createNewBookEl(title, author, pageCount, status) {
+function createNewBookEl(book, uid) {
     const newBook = document.createElement('div');
     newBook.classList.add('book');
-    newBook.setAttribute('data-title', title);
-    newBook.setAttribute('data-author', author);
-    newBook.setAttribute('data-page-count', pageCount);
-    newBook.setAttribute('data-read-status', status);
+    newBook.setAttribute('data-title', book.title);
+    newBook.setAttribute('data-author', book.author);
+    newBook.setAttribute('data-page-count', book.pageCount);
+    newBook.setAttribute('data-read-status', book.status);
+    newBook.setAttribute('data-uid', uid);
 
     const newBookContents = document.createElement('div');
     newBookContents.classList.add('book-contents');
@@ -100,8 +79,8 @@ function createNewBookEl(title, author, pageCount, status) {
     newBookStatus.classList.add('book-read-status');
 
     const newBookStatusTgl = document.createElement('input');
-    newBookStatusTgl.setAttribute('type','checkbox');
-    newBookStatusTgl.checked = status;
+    newBookStatusTgl.setAttribute('type', 'checkbox');
+    newBookStatusTgl.checked = book.status;
     newBookStatusTgl.classList.add('book-status-toggle');
     newBookStatusTgl.addEventListener('click', toggleStatus);
 
@@ -121,10 +100,10 @@ function createNewBookEl(title, author, pageCount, status) {
 
     newBookControls.appendChild(controlsRemoveBtn);
 
-    newBookTitle.textContent = title;
-    newBookAuthor.textContent = author;
-    newBookPageCount.textContent = pageCount;
-    newBookStatus.textContent = status;
+    newBookTitle.textContent = book.title;
+    newBookAuthor.textContent = book.author;
+    newBookPageCount.textContent = book.pageCount;
+    newBookStatus.textContent = book.status;
 
     newBook.appendChild(newBookContents);
     newBook.appendChild(newBookControls);
@@ -138,9 +117,9 @@ openBookModal.addEventListener('click', () => {
 
 submitBtn.addEventListener('click', submitNewBookHandler);
 
-addBookToLibrary(createNewBook('Atmic Habits', 'James Clear', 320, true));
-addBookToLibrary(createNewBook('Amic Habits', 'James Clear', 320, true));
-addBookToLibrary(createNewBook('Atomi Habits', 'James Clear', 320, true));
-addBookToLibrary(createNewBook('Atomic Habis', 'James Clear', 320, true));
-addBookToLibrary(createNewBook('Atomic Habits', 'James Clear', 320, true));
-updateBookList();
+addBookToLibrary('Atmiwec Hdfabits', 'James Clear', 320, true);
+addBookToLibrary('Atmic Hafifts', 'James Clear', 320, true);
+addBookToLibrary('Atmfc Hbits', 'James Clear', 320, true);
+addBookToLibrary('Atsic abitfs', 'James Clear', 320, true);
+addBookToLibrary('Atqwemic Habits', 'James Clear', 320, true);
+
